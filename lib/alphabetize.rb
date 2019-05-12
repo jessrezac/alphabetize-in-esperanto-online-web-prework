1c5a3277822dfd226e565a0c46e2ca7bc84b0e25
Jess Rezac <jess.rezac@gmail.com>
2019-05-12 23:28:17 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,7 +3,6 @@ def alphabetize(arr)
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
   arr.sort_by do |word|
-    word <=> esperanto_alphabet.scan(/./)
     esperanto_alphabet.split.index word.id
   end
 end
