135881f12093bd4e219f5c3c29c8c4095c550eed
Jess Rezac <jess.rezac@gmail.com>
2019-05-12 23:34:40 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,6 +2,5 @@ def alphabetize(arr)
   # code here
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   esperanto_alphabet = esperanto_alphabet.split("")
-  require 'pry'; binding.pry
   arr.index_by(&:id).values_at(*esperanto_alphabet)
 end
