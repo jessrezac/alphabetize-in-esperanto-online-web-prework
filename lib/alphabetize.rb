96c558a0c07343fff9a3c7ebe7b350a9eceb6157
Jess Rezac <jess.rezac@gmail.com>
2019-05-12 23:53:00 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,5 @@
 def alphabetize(arr)
   # code here
-  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  esperanto_alphabet = esperanto_alphabet.split("")
-  arr.sort_by { |phrase| phrase <=> esperanto_alphabet}
+  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by { |phrase| ESPERANTO_ALPHABET.split("").index(phrase)}
 end
