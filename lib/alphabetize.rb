c4223de56f15f3386c70084c27a3f6a8ecdf6555
Jess Rezac <jess.rezac@gmail.com>
2019-05-12 23:28:03 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,9 +4,7 @@ def alphabetize(arr)
 
   arr.sort_by do |word|
     word <=> esperanto_alphabet.scan(/./)
-
-    #    music.sort_by { |s| s.scan(/\d+/).first.to_i }
-    # ["5.mp3", "10.mp3", "21.mp3", "40.mp3"]
-    # I used a regular expression (\d+) to match the numbers, then get the first number (first) & convert it to an integer object (to_i).
+    esperanto_alphabet.split.index word.id
   end
 end
+terf
